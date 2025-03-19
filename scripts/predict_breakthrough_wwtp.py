0987c4d5c251e8673fed1c13c81b294186e308b7
corderjo <jose.cordero@eawag.ch>
2025-03-19 14:58:58 +0100

    TEST: Jose's first check; installation succesful and main scripts

--- scripts/predict_breakthrough_wwtp.py
+++ scripts/predict_breakthrough_wwtp.py
@@ -11,10 +11,10 @@ if __name__ == '__main__':
     # ------------------------------------#
     # Initialize with user preferences  #
     # ------------------------------------#
-    pep = Pepper()
+    pep = Pepper(renku=True)
     pep.set_setup_name('example')
     pep.set_data_type('WWTP')
-    pep.set_tag('full_database')
+    pep.set_tag('combined_data')
     pep.set_target_variable_name('logB')
     pep.set_target_variable_std_name('stdev')
     pep.set_smiles_name('CanonicalSMILES')
@@ -23,29 +23,29 @@ if __name__ == '__main__':
     # Data import, curation and analysis #
     # ------------------------------------#
     wwtp_data = DataStructureWWTP(pep)
-    wwtp_data.load_data('name_data')
-    wwtp_data.load_data('full_data')
-    wwtp_data.set_curation_type('complete_curation_nitrification_plants_only')
+    wwtp_data.load_data('name_data', source='data')
+    wwtp_data.load_data('full_data', source='data')
+    wwtp_data.set_curation_type('testing_pypi_version')
     wwtp_data.set_plant_list(wwtp_data.ndn_plants)
     wwtp_data.curate_by_target_variable()
     wwtp_data.set_id_name('Combined_ID')
     wwtp_data.reduce_for_modelling(no_curation=False,
-                                   avoid_sorbing_and_volatile=True, only_enough_data=True,
-                                   only_above_LOQ=True, no_formation=True, avoid_high_std=True)
+                                   avoid_sorbing_and_volatile=False, only_enough_data=True,
+                                   only_above_LOQ=False, no_formation=False, avoid_high_std=True)
     wwtp_data.select_modeling_data()
     wwtp_data.create_modelling_input()
-    # wwtp_data.load_data('model_data')
-    # wwtp_data.randomize_y()
-
-    # ------------------------------------#
-    # Descriptor calculation              #
-    # ------------------------------------#
-    # calculate descriptors
+    # # wwtp_data.load_data('model_data')
+    # # wwtp_data.randomize_y()
+    #
+    # # ------------------------------------#
+    # # Descriptor calculation              #
+    # # ------------------------------------#
+    # # calculate descriptors
     descriptors = Descriptors(pep)
     descriptors.set_data(wwtp_data)
-    descriptors.load_descriptors(from_csv=True, MACCS=True, mordred=True, enviPath_trig=True)
-    # descriptors.generate_pseudodescriptors()
-
+    descriptors.load_descriptors(from_csv=False, MACCS=True, mordred=True)
+    # # descriptors.generate_pseudodescriptors()
+    #
     # ------------------------------------#
     # Modeling                            #
     # ------------------------------------#
