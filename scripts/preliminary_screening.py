49c4330390d114736ac8e40e6e1c45b5b2dccbbf
Cordero <jose.cordero@eawag.ch>
2025-07-03 18:30:35 +0200

    tried running the preliminarz screening on windows, it does not recogniye the path

---
+++ scripts/preliminary_screening.py
@@ -0,0 +1,71 @@
+"""Changelog in comparison to predict_breakthrough_wwtp.py:
+
+- No additional curation of training data
+- defulat hyperparameters
+- complete chemical space (padel, mordred, maccs, ecfp, rdkit, ep_trig)
+- Load descriptors from_csv=True (added them manually to avoid fetching them from envipath which takes time)
+- All regressors (MLR, Ridge, KRidge, SGD, LSVR, RF, AB, DT, MLP, SVR, KNN)
+-  Where is GBR? and GPR? 
+
+
+"""
+
+from pepper_lab.pepper import Pepper
+from pepper_lab.datastructurewwtp import DataStructureWWTP
+from pepper_lab.descriptors import Descriptors
+from pepper_lab.modeling import Modeling
+from pepper_lab.visualize import Visualize
+
+
+if __name__ == '__main__':
+    # ------------------------------------#
+    # Initialize with user preferences  #
+    # ------------------------------------#
+    pep = Pepper(renku=True)
+    pep.set_setup_name('example')
+    pep.set_data_type('WWTP')
+    pep.set_tag('combined_data')
+    pep.set_target_variable_name('logB')
+    pep.set_target_variable_std_name('stdev')
+    pep.set_smiles_name('CanonicalSMILES')
+
+    # ------------------------------------#
+    # Data import, curation and analysis #
+    # ------------------------------------#
+    wwtp_data = DataStructureWWTP(pep)
+    wwtp_data.load_data('name_data', source='data')
+    wwtp_data.load_data('full_data', source='data')
+    wwtp_data.set_curation_type('basic_curation_nitrification_plants_only_newtest20250701')
+    wwtp_data.set_plant_list(wwtp_data.ndn_plants)
+    wwtp_data.curate_by_target_variable()
+    wwtp_data.set_id_name('Combined_ID')
+    wwtp_data.reduce_for_modelling(no_curation=True)
+    wwtp_data.select_modeling_data()
+    wwtp_data.create_modelling_input()
+    print("Data loaded and curated.")
+
+    # ------------------------------------#
+    # Descriptor calculation              #
+    # ------------------------------------#
+    # calculate descriptors
+    descriptors = Descriptors(pep)
+    descriptors.set_data(wwtp_data)
+    descriptors.load_descriptors(from_csv=True)
+    print("Descriptors loaded.")
+
+    # ------------------------------------#
+    # Modeling                            #
+    # ------------------------------------#
+
+    wwtp_modeling = Modeling(pep, wwtp_data, descriptors, config='default_wwtp')
+    print("Modeling initialized.")
+    # 
+    # desired_wwtp_model = wwtp_modeling.build_final_model(regressor_name='RF',
+    #                                                      feature_space='maccs', config='wwtp_optimized')
+    # # Alternative option
+    # # wwtp_modeling.nested_cross_val_screening(feature_space_list=['maccs',
+    # #                                                               'maccs+ep_trig',
+    # #                                                               'mordred',
+    # #                                                               'all'],
+    # #                                                                config='default_wwtp')
+    print('done')
